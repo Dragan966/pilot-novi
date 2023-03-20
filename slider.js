8d2a03b024a1180402b3aa882771ef3bfc986db2
nickderm <119490937+nickderm@users.noreply.github.com>
2023-03-20 01:39:53 +0100

    funkcionalan slajder
    radi sve ali mnogo masi kada se ide na strelice

--- slider.js
+++ slider.js
@@ -1,11 +1,48 @@
 const slider = document.querySelector('.slider-items');
+const slide = document.querySelector('.sliderItem');
 // const progressBar = document.querySelector('.prog-bar-inner');
-// slider.parentElement.clientWidth =  10;
-console.log(slider.scrollLeft);
+const leftBtnMain = document.querySelector('#leftMainSlider');
+const rightBtnMain = document.querySelector('#rightMainSlider');
+
+// pripremanje niza
+let slidesPositions = [0];
+let numberOfSlides = 10;
+// slidesPositions.push(slide.offsetWidth / 2);
+slidesPositions.push(345);
+do {
+    slidesPositions.push(slidesPositions[slidesPositions.length - 1] + slide.offsetWidth);
+} while (slidesPositions.length < numberOfSlides - 1)
+slidesPositions.push(slider.parentElement.scrollWidth);
+console.log(slidesPositions);
+
+let currentSlide = 1;
+slider.parentElement.scrollLeft = slidesPositions[currentSlide];
+leftBtnMain.addEventListener('click', () => {
+    if(currentSlide > 0) {
+        --currentSlide;
+        slider.parentElement.scrollLeft = slidesPositions[currentSlide];
+    }
+});
+
+
+rightBtnMain.addEventListener('click', () => {
+    if(currentSlide < numberOfSlides - 1) {
+        ++currentSlide;
+        slider.parentElement.scrollLeft = slidesPositions[currentSlide];
+    }
+});
+// OVO RADI POSAO 
+// slider.parentElement.scrollLeft += 500;
+// slider.parentElement.scrollLeft += 0;
+// console.log(`sirina diva: ${pojedinacni.offsetWidth}`)
+// console.log(slider.scrollLeft);
+// console.log(slider.parentElement.scrollWidth);
+// console.log(slider.parentElement.clientWidth);
 let sliderGrabbed = false;
 
 slider.parentElement.addEventListener('scroll', (e) => {
-    //console.log(getScrollPercentage());
+    // console.log(getScrollPercentage());
+    console.log(slider.parentElement.scrollLeft);
 })
 
 slider.addEventListener('mousedown', (e) => {
@@ -36,3 +73,7 @@ slider.addEventListener('wheel', (e) =>{
 function getScrollPercentage(){
    return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
 }
+
+function proba(broj){
+    return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
+ }
