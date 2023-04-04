e1f98ec150662468d64666a5b5d834265b9a82ff
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-04 15:56:37 +0200

    zakljucavanje overflow-a
    klikom na hamburger menu zakljucava se overflow na body-ju i scroll se pali na nav > .container > ul

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburgerIconDIV = document.querySelector('.hamburgerIcon');
 const hamburgerIcon = document.querySelector('.hamburgerIcon > img');
 const navigation = document.querySelector('nav > .container > ul');
+const body = document.querySelector('body');
 
 hamburgerIcon.addEventListener('click', mobileMenu);
 
@@ -8,9 +9,13 @@ function mobileMenu() {
     if(navigation.style.left !== '0%') {
         navigation.style.left = '0%';
         hamburgerIcon.src = 'SLIKE/close_FILL0_wght400_GRAD0_opsz48.svg';
+        navigation.style.overflow = 'scroll';
+        body.style.overflow = 'hidden';
     } else {
         navigation.style.left = '-110%';
         hamburgerIcon.src = 'SLIKE/menu_FILL0_wght700_GRAD200_opsz48.svg';
+        navigation.style.overflow = 'hidden';
+        body.style.overflow = 'scroll';
     }
     
 }
