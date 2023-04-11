dfebb21b161bf59d01f74b171ce77844c6863bbb
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-12 01:28:08 +0200

    scroll to top, zatvara sve

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ hamburgerIcon.addEventListener('click', mobileMenu);
 // prikazivanje/sakrivanje dropDown menija na telefonu
 if (window.innerWidth < 1024) {
     h3.forEach(h3 => {
+        h3.classList.toggle('marker0');
         h3.addEventListener('click', () => { 
             h3.classList.toggle('marker0');
             const ulArr = Array.from(h3.parentNode.parentNode.children);
@@ -19,6 +20,21 @@ if (window.innerWidth < 1024) {
                 expand(li);
                 li.classList.toggle('marker1');
             });
+            if(h3.classList.contains('marker0')) {
+                const proba = h3.parentNode.parentNode;
+                const opened1 = Array.from(proba.querySelectorAll('.marker1'));
+                // console.log(opened1);
+                const opened2 = Array.from(proba.querySelectorAll('.marker2'));
+                // console.log(opened2);
+                opened2.forEach(o => {
+                    expand(o);
+                    o.classList.toggle('marker2');
+                });
+                opened1.forEach(o => {
+                    expand(o);
+                    o.classList.toggle('marker1');
+                });
+            }
         });
     });
     
@@ -48,6 +64,7 @@ function expand (node) {
 function mobileMenu() {
     if(navigation.style.left !== '0%') {
         navigation.style.left = '0%';
+        navigation.scrollTop = 0;
         hamburgerIcon.src = 'SLIKE/close_FILL0_wght400_GRAD0_opsz48.svg';
         navigation.style.overflow = 'scroll';
         body.style.overflow = 'hidden';
