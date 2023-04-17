35e642c50f50918af0701397614cb92d5186c148
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-17 02:36:21 +0200

    pozadina je iskrivljena kako treba

---
+++ proba.js
@@ -0,0 +1,14 @@
+// const statsInnerBack = document.querySelector('.statsInnerBack');
+// const upTriangle = window.getComputedStyle(statsInnerBack, '::before');
+// const loTriangle = window.getComputedStyle(statsInnerBack, '::after');
+
+// function resize() {
+//     statsInnerBack.style.setProperty('--windowSize', window.innerWidth + 'px');
+//     loTriangle.borderLeft = window.innerWidth + 'px solid transparent';
+// }
+
+// resize();
+
+// document.documentElement.style.setProperty('--windowWidth', window.innerWidth + 'px');
+
+// document.documentElement.style.setProperty('--proba', 'green');
