31371f02f900a86009d57d1b59e12353be9b6d6f
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-17 17:59:43 +0200

    ok

--- statsPresentation.js
+++ statsPresentation.js
@@ -1,53 +1,221 @@
-const statsCircle = document.querySelectorAll('.statsCircle .stat');
-const h1 = document.querySelector('.statsCircle h1');
-const para = document.querySelector('.statsCircle p');
-let interval;
-let i = 0;
+const statsCircle = document.querySelector('.statsCircle');
+const allCircles = document.querySelectorAll('.statsCircle > .stat');
+const centerInfo = document.querySelector('.statsCircle > .center');
+const infoNumber = document.querySelector('.statsCircle > .center > h1');
+const infoPara = document.querySelector('.statsCircle > .center > p');
+let intervalInfo;
+let INFO = 0;
 
-const stats = [
+const animations = [
     {
-        number: 1,
-        text: 'Prva tekst neki ssdas',
-    },
+        animationName : 'dealingOne',
+        top: 0 + '%',
+        left: 42.84 + '%',
+    }, 
+    
     {
-        number: 2,
-        text: 'Druga tekst neki ssdas',
+        animationName : 'dealingTwo',
+        top: 13.3 + '%',
+        right: 13.3 + '%',
     },
+
     {
-        number: 3,
-        text: 'Treca tekst neki ssdas',
+        animationName : 'dealingThree',
+        top: 42.84 + '%',
+        right: 0 + '%',
     },
+
     {
-        number: 4,
-        text: 'Cetvrta tekst neki ssdas',
+        animationName : 'dealingFour',
+        right: 13.3 + '%',
+        bottom: 13.3 + '%',
     },
+
     {
-        number: 5,
-        text: 'Peta tekst neki ssdas',
+        animationName : 'dealingFive',
+        bottom: 0 + '%',
+        left: 42.84 + '%',
     },
+
     {
-        number: 6,
-        text: 'Sesta tekst neki ssdas',
+        animationName : 'dealingSix',
+        bottom: 13.3 + '%',
+        left: 13.3 + '%',
     },
+
     {
-        number: 7,
-        text: 'Sedma tekst neki ssdas',
+        animationName : 'dealingSeven',
+        top: 42.84 + '%',
+        left: 0 + '%',
     },
+
     {
-        number: 8,
-        text: 'Osma tekst neki ssdas',
+        animationName : 'dealingEight',
+        top: 13.3 + '%',
+        left: 13.3 + '%',
     },
+
 ];
 
 
-function changeDot() {
-    if (!interval) {
-      interval = setInterval(linksOrg, 4500);
+const allInfo = [
+    {
+        number: '1',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '2',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '3',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '4',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '5',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '6',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '7',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+
+    {
+        number: '8',
+        text: 'Phasellus nec nunc purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.'
+    },
+];
+
+const allCirclesArr = Array.from(allCircles);
+
+function handleIntersection(entries) {
+    entries.forEach(entry => {
+    if (entry.isIntersecting) {
+       
+
+        allCirclesArr.forEach((circle, index) => {
+            setTimeout(() => {
+                circle.style.opacity = 1;
+
+                for (const prop in animations[index]) {
+                    circle.style[prop] = animations[index][prop];
+                }
+
+            }, index * 500);
+            
+        });
+
+        allCirclesArr[7].addEventListener('animationend', () => {
+            console.log('gotova animacija');
+            centerInfo.style.opacity = 1;
+
+            startNextCircle();
+
+            allCirclesArr.forEach((circle, index) => {
+                circle.addEventListener('mouseenter', () => {
+                    console.log("stopirao")
+                    stopNextCircle();
+
+                    infoNumber.innerHTML = allInfo[index].number;
+                    infoPara.innerHTML = allInfo[index].text;
+                    circle.style.backgroundColor = '#0C71C3';                 
+                });
+            
+                circle.addEventListener('mouseleave', () => {
+                    console.log('krece opet');
+                    startNextCircle();
+                    circle.style.backgroundColor = 'white';
+                });
+            });
+
+        });
+
+    } else {
+        centerInfo.style.opacity = 0;
+        infoNumber.innerHTML = '';
+        infoPara.innerHTML = '';
+        INFO = 0;
+        stopNextCircle();
+
+        allCirclesArr.forEach((circle, index) => {
+            circle.style.opacity = 0;
+            circle.style.animationName = 'none';
+
+            if(animations[index].hasOwnProperty('top')) {
+                circle.style.top = '42.84%';
+            }
+
+            if(animations[index].hasOwnProperty('right')) {
+                circle.style.right = '42.84%';
+            }
+
+            if(animations[index].hasOwnProperty('bottom')) {
+                circle.style.bottom = '42.84%';
+            }
+
+            if(animations[index].hasOwnProperty('left')) {
+                circle.style.left = '42.84%';
+            }
+        });
+
+    }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    threshold: 1.0
+});
+
+observer.observe(statsCircle);
+
+
+function startNextCircle() {
+    if (!intervalInfo) {
+        intervalInfo = setInterval(afterCircleAnimation, 1500);
     }
 }
   
-function stopDot() {
-    clearInterval(interval);
-    interval = null;
+function stopNextCircle() {
+    clearInterval(intervalInfo);
+    intervalInfo = null;
 }
 
+function nextCircle(index) {
+    infoNumber.innerHTML = allInfo[index].number;
+    infoPara.innerHTML = allInfo[index].text;
+
+    allCirclesArr[index].style.backgroundColor = '#0C71C3';
+    setTimeout(() => {
+        allCirclesArr[index].style.backgroundColor = 'white';
+    }, 1000);
+}
+
+function afterCircleAnimation() {
+    nextCircle(INFO);
+    INFO++;
+    if(INFO > 7) {
+        INFO = 0;
+    }
+}
+
+// bigPhoto.parentElement.parentElement.addEventListener('mouseenter', () => {
+//     stopchangeBigPicture();
+// });
+
+// bigPhoto.parentElement.parentElement.addEventListener('mouseleave', () => {
+//     changeBigPicture();
+// });
