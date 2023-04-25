33d8a7a94b0caab41d769519765ca21216a7cc54
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-26 01:27:31 +0200

    gotov minimap na teme koje istrazujemo

--- JS/miniMap.js
+++ JS/miniMap.js
@@ -72,6 +72,60 @@ const karticaUstanove = [
 ];
 
 
+
+const karticaTeme = [
+    {
+        naziv: ['Inkluzivno-obrazovanje', 'Инклузивно образовање'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Inkluzivno-obrazovanje/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Inkluzivno-obrazovanje/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Inkluzivno-obrazovanje/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Inkluzivno-obrazovanje/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Prevencija-nasilja', 'Превенција насиља'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Prevencija-nasilja/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Prevencija-nasilja/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Prevencija-nasilja/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Prevencija-nasilja/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Razvojno-planiranje', 'Развојно планирање'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Razvojno-planiranje/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Razvojno-planiranje/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Razvojno-planiranje/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Razvojno-planiranje/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Standardi-u-obrazovanju', 'Стандарди у образовању'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Standardi-u-obrazovanju/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Standardi-u-obrazovanju/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Standardi-u-obrazovanju/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Standardi-u-obrazovanju/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Samovrednovanje-i-vrednovanje-ustanova', 'Самовредновање и вредновање установа'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Samovrednovanje-i-vrednovanje-ustanova/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Samovrednovanje-i-vrednovanje-ustanova/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Samovrednovanje-i-vrednovanje-ustanova/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Samovrednovanje-i-vrednovanje-ustanova/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Strucno-predagoski-nadzor', 'Стручно-педагошки надзор'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Strucno-predagoski-nadzor/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Strucno-predagoski-nadzor/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Strucno-predagoski-nadzor/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Strucno-predagoski-nadzor/Pitanja-i-odgovori.html'],
+    },
+    {
+        naziv: ['Strucno-usavrsavanje-i-sticanje-zvanja', 'Стручно усавршавање и стицање звања'],
+        Zakonska: ['Законска регулатива', '../../Teme-koje-istrazujemo/Strucno-usavrsavanje-i-sticanje-zvanja/Zakonska-regulativa.html'],
+        Dodatni: ['Додатни извори знања', '../../Teme-koje-istrazujemo/Strucno-usavrsavanje-i-sticanje-zvanja/Dodatni-izvori-znanja.html'],
+        Nasa: ['Наша искуства и примери добре праксе', '../../Teme-koje-istrazujemo/Strucno-usavrsavanje-i-sticanje-zvanja/Nasa-iskustva-i-primeri-dobre-prakse.html'],
+        Pitanja: ['Питања и одговори', '../../Teme-koje-istrazujemo/Strucno-usavrsavanje-i-sticanje-zvanja/Pitanja-i-odgovori.html'],
+    },
+];
+
+
 const miniMaph3 = document.querySelector('.miniMap > h3');
 const miniMapul = document.querySelector('.miniMap > ul');
 
@@ -88,6 +142,20 @@ if(currentURL.includes('Ustanove')) {
                     miniMaph3.innerHTML = opsitnaInfo[key][1];
                 } else if(opsitnaInfo[key]){
                     addingLi(opsitnaInfo[key][0], opsitnaInfo[key][1]);
+                }
+            }
+        }
+    })
+} else if(currentURL.includes('Teme-koje-istrazujemo')) {
+    karticaTeme.forEach(temeInfo => {
+        if(currentURL.includes(temeInfo.naziv[0])){
+            for (const key in temeInfo) {
+                if (currentURL.includes(key)) {
+                    addingLi(temeInfo[key][0], '#', 1);
+                } else if(key === 'naziv'){
+                    miniMaph3.innerHTML = temeInfo[key][1];
+                } else if(temeInfo[key]){
+                    addingLi(temeInfo[key][0], temeInfo[key][1]);
                 }
             }
         }
