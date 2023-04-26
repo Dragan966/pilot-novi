f0e7b0c06c4fcb76df05345ce6d41f98cfeda973
nickderm <119490937+nickderm@users.noreply.github.com>
2023-04-26 02:39:04 +0200

    dodat click to copy

--- JS/zaposleni-script.js
+++ JS/zaposleni-script.js
@@ -1,22 +1,8 @@
-const allProfiles = document.querySelectorAll('.allProfiles .profile');
+const allProfiles = document.querySelectorAll('.person-info p:last-of-type');
 
 allProfiles.forEach(profile => {
-    let wide = false;
     profile.addEventListener('click', () => {
-        
-        console.log(profile);
-        console.log(wide);
-        if (window.innerWidth > 1024) {
-            if(wide) {
-                wide = false;
-                profile.style.width = '300px';
-            } else {
-                wide = true;
-                profile.style.width = '640px';
-            }
-        
-         } else {
-
-         }
+        console.log(profile.innerHTML);
+        navigator.clipboard.writeText(profile.innerHTML);
     });
 });
