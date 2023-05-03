d287471d070af3cece7c2333d45daed63d5a6ca5
nickderm <119490937+nickderm@users.noreply.github.com>
2023-05-03 20:00:56 +0200

    search kao bolje radi

--- JS/getJsonFiles.js
+++ JS/getJsonFiles.js
@@ -678,33 +678,97 @@ function Lokacija(name, link) {
   this.name = [name];
   this.name.push(prevodjenje(name));
   this.link = link;
+  this.score = 0;
 }
 
 function searchLogic() {
   const searchWord = document.querySelector('#search-word');
-  let word = prevodjenje(queryParameters('key'));
+  let fullword = prevodjenje(queryParameters('key'));
 
+  const wordArr = fullword.split(' ');
+  
+  let results = [];
+  wordArr.forEach(word => {
+    if(word.length >= 3) {
+      word = word.substring(0, word.length - 1);
+      console.log(word);
+    }
+    const oneWordResults = navLokacijeFull.filter(element => element.name[1].includes(word));
+    results = results.concat(oneWordResults);
+  });
+  
+  duplicates(results);
+  results.forEach(element => {
+    element.score = similarity(element.name[1], fullword);
+  });
+  results.sort((a, b) => b.score - a.score);
+  console.log(results);
+    if(queryParameters('key')) {
+      searchWord.innerHTML = queryParameters('key') + `  (${results.length} резултата)`;
 
-  if(queryParameters('key')) {
-    const results = navLokacijeFull.filter(element => element.name[1].includes(word));
-    searchWord.innerHTML = queryParameters('key') + `  (${results.length} резултата)`;
-
-    if (document.readyState === "interactive" || document.readyState === "complete") {
-      results.forEach(element => {
-        searchOneResult(element);
-      });
-    } else {
-      document.addEventListener("DOMContentLoaded", function() {
+      if (document.readyState === "interactive" || document.readyState === "complete") {
         results.forEach(element => {
           searchOneResult(element);
         });
-      });
+      } else {
+        document.addEventListener("DOMContentLoaded", function() {
+          results.forEach(element => {
+            searchOneResult(element);
+          });
+        });
+      }
+    } else {
+      searchWord.innerHTML =  'непознат унос  (0 резултата)';
     }
+}
 
-    // results.forEach(element => {
-    //   searchOneResult(element);
-    // });
-  } else {
-    searchWord.innerHTML =  'непознат унос  (0 резултата)';
+function duplicates(array) {
+  for(let i = 0; i < array.length; ++i) {
+    for(let j = i + 1; j < array.length; ++j) {
+        if(array[i] === array[j]) {
+          array.splice(j--, 1);
+        }  
+    }
   }
+}
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
 }
